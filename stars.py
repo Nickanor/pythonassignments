784046d05f6e3760b3b5e0b1fb72c94e78494fc2
Nicholas Harvey <nicholasharvey10@gmail.com>
2016-06-09 12:53:29 -0700

    Part Two

--- stars.py
+++ stars.py
@@ -1,7 +1,18 @@
-def draw_stars(mylist):
-    for number in mylist:
-        print('*' * number)
+# def draw_stars(mylist):
+#     for number in mylist:
+#         print('*' * number)
+#
+# x = [4, 6, 1, 3, 5, 7, 25]
+#
+# draw_stars(x)
 
-x = [4, 6, 1, 3, 5, 7, 25]
+
+def draw_stars(mylist):
+    for item in mylist:
+        if (type(item) == int):
+            print('*' * item)
+        elif(type(item) == str):
+            print(item[0].lower() * len(item))
+x = [4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"]
 
 draw_stars(x)
