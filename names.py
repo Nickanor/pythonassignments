76c69e8056278412162208f2c7a89bff174f559b
Nicholas Harvey <nicholasharvey10@gmail.com>
2016-06-10 14:12:04 -0700

    Not Finished

--- names.py
+++ names.py
@@ -1,8 +1,27 @@
-students = [
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'},
+#      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#      {'first_name' : 'KB', 'last_name' : 'Tonel'}
+# ]
+# for value in students:
+#     print value["first_name"],value["last_name"]
+
+
+users = {
+ 'Students': [
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
      {'first_name' : 'John', 'last_name' : 'Rosales'},
      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
      {'first_name' : 'KB', 'last_name' : 'Tonel'}
-]
-for value in students:
-    print value["first_name"],value["last_name"]
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for keys in users:
+    print(keys)
+    for prop in users[keys]:
+        print prop["id"],"-",prop["first_name"],prop["last_name"],"-",len(prop["first_name"])+len(prop["last_name"])
