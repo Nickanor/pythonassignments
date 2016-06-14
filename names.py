de2bf2d870128fe62f8f86b10f6d10fe2e903db7
Nicholas Harvey <nicholasharvey10@gmail.com>
2016-06-14 12:26:18 -0700

    Final

--- names.py
+++ names.py
@@ -10,18 +10,18 @@
 
 users = {
  'Students': [
-     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-     {'first_name' : 'John', 'last_name' : 'Rosales'},
-     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+     {'num': 1, 'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'num': 2, 'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'num': 3, 'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'num': 4, 'first_name' : 'KB', 'last_name' : 'Tonel'}
   ],
  'Instructors': [
-     {'first_name' : 'Michael', 'last_name' : 'Choi'},
-     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+     {'num': 1, 'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'num': 2, 'first_name' : 'Martin', 'last_name' : 'Puryear'}
   ]
  }
 
 for keys in users:
     print(keys)
-    for prop in users[keys]:
-        print prop["id"],"-",prop["first_name"],prop["last_name"],"-",len(prop["first_name"])+len(prop["last_name"])
+    for value in users[keys]:
+        print value["num"], "-", value["first_name"], value["last_name"],"-", len(value["first_name"]) + len(value["last_name"])
